8ebaff38aaa72f4e4b3e5776ab9fda7e988bee49
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-04-09 03:36:27 -0400

    Adapted Javascript Code to be reusable

--- website-menu/src/index.js
+++ website-menu/src/index.js
@@ -1,11 +1,13 @@
 function addClickEventToChevron() {
-  const chevron = document.querySelector('.fa-chevron-down');
-  const dropdownContent = document.querySelector('.dropdown-content');
-  chevron.addEventListener('click', () => {
-    chevron.classList.toggle('rotated');
-    dropdownContent.classList.toggle('hidden');
+  const dropdownToggles = document.querySelectorAll('.fa-chevron-down');
+
+  dropdownToggles.forEach((toggle) => {
+    toggle.addEventListener('click', function () {
+      const dropdownContent = this.parentElement.nextElementSibling;
+      this.classList.toggle('rotated');
+      dropdownContent.classList.toggle('hidden');
+    });
   });
 }
 
-// Add DOMContentLoaded event listener to call manipulateDOM
 document.addEventListener('DOMContentLoaded', addClickEventToChevron);
